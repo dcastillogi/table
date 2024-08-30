272ee156a37bfe421a987eee122bbefb9fe5bb4f
Daniel Castillo <dev@dcastillogi.com>
2024-08-29 21:37:35 -0500

    Static mode

--- www/next.config.mjs
+++ www/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export', // Export in static mode
+    distDir: 'build', // Output directory
+
+};
 
 export default nextConfig;
